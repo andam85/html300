104dbee876813a0bffc2cd02fb0798cca010d964
Duy-An Q Dam <duy-an.q.dam@boeing.com>
2019-04-22 08:38:05 -0700

    added file from in class activity

--- lesson03/activity/index.js
+++ lesson03/activity/index.js
@@ -1,13 +1,1 @@
-const dog = {
-  name: 'ted',
-  breed: 'poodle',
-  color: 'brown',
-  barkLevel : 7,
-  energyLevel : 2,
-  patMethod: function(){
-       return this.energyLevel+1;
-  };
 
-  }
-
-console.log(dog.breed);
